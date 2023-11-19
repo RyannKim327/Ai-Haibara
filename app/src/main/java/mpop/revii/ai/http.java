deb164cfad0f5775e41e85ba4a4a445053db65d3
RyannKim327 <rksesgundo123@gmail.com>
2023-11-19 22:42:25 +0800

    11/19/2023 10:42:24 PM [Autogit]

--- app/src/main/java/mpop/revii/ai/http.java
+++ app/src/main/java/mpop/revii/ai/http.java
@@ -49,9 +49,10 @@ public class http extends AsyncTask {
 			JSONObject obj = new JSONObject(result.toString());
 			Intent i = new Intent("mpop.revii.ai.DATA");
 			if(obj.getString("reply").equalsIgnoreCase(url)){
-
+				i.putExtra("DATA", "Something went wrong");
 			}else{
 				i.putExtra("DATA", obj.getString("reply"));
+			}
 			ctx.sendBroadcast(i);
 		} catch (JSONException e) {
 			Intent i = new Intent("mpop.revii.ai.DATA");
