07fc7444a2dbf095fba0ab1b00a465b0c187a706
RyannKim327 <rksesgundo123@gmail.com>
2023-11-19 22:21:36 +0800

    11/19/2023 10:21:36 PM [Autogit]

--- app/src/main/java/mpop/revii/ai/MainActivity.java
+++ app/src/main/java/mpop/revii/ai/MainActivity.java
@@ -7,6 +7,9 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        getActionBar().setIcon(AI.setResources(this, "ic_launcher", "drawable"));
+		getActionBar().setTitle("Bebe ang AI");
+		getActionBar().setSubtitle("Developed by RyannKim327");
 		setContentView(R.layout.activity_main);
 		
 	}
