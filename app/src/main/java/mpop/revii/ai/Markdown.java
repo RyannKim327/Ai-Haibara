dc1355227959ed32105f9730dc03aad065780afa
RyannKim327 <rksesgundo123@gmail.com>
2023-11-20 10:36:00 +0800

    11/20/2023 10:36:00 AM [Autogit]

--- app/src/main/java/mpop/revii/ai/Markdown.java
+++ app/src/main/java/mpop/revii/ai/Markdown.java
@@ -18,7 +18,7 @@ public class Markdown extends TextView {
 			if(txt.startsWith("```")){
 				x = !x;
 				if(!x){
-					result += "<font color=\"#000370\">";
+					result += "<font color=\"#aaaaaa\">";
 				}else{
 					result += "</font>";
 				}
