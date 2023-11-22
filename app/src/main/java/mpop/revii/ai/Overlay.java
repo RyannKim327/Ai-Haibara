c075ca4cd23ba75c6ebb08a007f885bba9938df9
RyannKim327 <rksesgundo123@gmail.com>
2023-11-22 20:06:26 +0800

    11/22/2023 8:06:26 PM [Autogit]

--- app/src/main/java/mpop/revii/ai/Overlay.java
+++ app/src/main/java/mpop/revii/ai/Overlay.java
@@ -1,2 +1,12 @@
-package mpop.revii.ai;public class Overlay {
+package mpop.revii.ai;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Overlay extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
