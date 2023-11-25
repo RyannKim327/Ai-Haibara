c99b987e1beac9835aa29f199121d3f5ff0f1520
RyannKim327 <rksesgundo123@gmail.com>
2023-11-25 21:22:41 +0800

    11/25/2023 9:22:41 PM [Autogit]

--- app/src/main/java/mpop/revii/ai/Overlay.java
+++ app/src/main/java/mpop/revii/ai/Overlay.java
@@ -89,13 +89,13 @@ public class Overlay extends Service {
 						// y = params.y;
 						// X = motionEvent.getRawX();
 						// Y = motionEvent.getRawY();
+						moved = paramsImg.x != Math.round(x + (motionEvent.getRawX() - X)) || paramsImg.y != Math.round(y + (motionEvent.getRawY() - Y));
 						paramsImg.x = Math.round(x + (motionEvent.getRawX() - X));
 						paramsImg.y = Math.round(y + (motionEvent.getRawY() - Y));
 						managerImg.updateViewLayout(img, paramsImg);
-						moved = true;
 						return true;
 				}
-				if(moved){
+				if(!moved){
 					if(!show) {
 						posX = paramsImg.x;
 						posY = paramsImg.y;
