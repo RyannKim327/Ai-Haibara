208f122cd972d673f89f196a0ebba51384cdc9a0
RyannKim327 <rksesgundo123@gmail.com>
2023-12-02 19:33:11 +0800

    12/2/2023 7:33:10 PM [Autogit]

--- app/src/main/java/mpop/revii/ai/AI.java
+++ app/src/main/java/mpop/revii/ai/AI.java
@@ -239,12 +239,13 @@ public class AI extends LinearLayout implements TextToSpeech.OnInitListener {
 			public void onEvent(int i, Bundle bundle) {}
 		});
 
-		if(sr)
-		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en_US");
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-		sr.startListening();
-
+		if(sr != null) {
+			Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+			intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en_US");
+			intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+			intent.putExtra()
+			sr.startListening(intent);
+		}
 		input.addView(e);
 		input.addView(v);
 		input.addView(iv);
