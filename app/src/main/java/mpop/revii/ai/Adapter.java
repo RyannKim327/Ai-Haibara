1cdf0ed99f8fa43f158011986631ceea19aa5284
RyannKim327 <rksesgundo123@gmail.com>
2023-12-08 03:43:14 +0800

    12/8/2023 3:43:13 AM [Autogit]

--- app/src/main/java/mpop/revii/ai/Adapter.java
+++ app/src/main/java/mpop/revii/ai/Adapter.java
@@ -1,2 +1,16 @@
-package mpop.revii.ai;public class Adapter {
+package mpop.revii.ai;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class Adapter extends ArrayAdapter {
+	Context ctx;
+	ArrayList<String> list;
+	public Adapter(Context context, ArrayList arraylist){
+		super(context, util.setResources(context, "layout_list", "layout"), arraylist);
+		ctx = context;
+		list = arraylist;
+	}
 }
