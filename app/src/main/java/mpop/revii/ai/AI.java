56a3838578f284253ea0d3d5cc0fae456789c58a
RyannKim327 <rksesgundo123@gmail.com>
2023-12-08 16:45:49 +0800

    12/8/2023 4:45:49 PM [Autogit]

--- app/src/main/java/mpop/revii/ai/AI.java
+++ app/src/main/java/mpop/revii/ai/AI.java
@@ -359,7 +359,7 @@ public class AI extends LinearLayout implements TextToSpeech.OnInitListener {
 							@Override
 							public void onClick(DialogInterface dialogInterface, int i) {
 								((ClipboardManager) ctx.getSystemService(ctx.CLIPBOARD_SERVICE)).setText(list.fetchItem(i));
-								util.show(ctx, "Text Copied " + list.fetchItem(i));
+								util.show(ctx, "Text Copied");
 							}
 						});
 					} else {
